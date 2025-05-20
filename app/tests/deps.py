a59769b11f182bbb20cd4388ee656ba0318a82ab
Ivan Dranovskiy <vanyadranovskiy@gmail.com>
2025-05-20 13:41:00 +0300

    add tests for auth and login

---
+++ app/tests/deps.py
@@ -0,0 +1,25 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.db.base import Base
+
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///"
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+TestingSessionLocal = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+async def init_test_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+async def get_test_db() -> AsyncGenerator[AsyncSession, None]:
+    async with TestingSessionLocal() as session:
+        yield session 
